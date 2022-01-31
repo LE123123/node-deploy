fbfdf02219f45e45687cf3046e923f7681972dd9
LE123123 <heart20021010@gmail.com>
2022-01-31 18:58:47 +0900

    third commit

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,8 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const AWS = require("aws-sdk");
+const multerS3 = require("multer-s3");
 
 const { Post, Hashtag, User } = require("../models");
 const { isLoggedIn } = require("./middlewares");
@@ -19,26 +21,44 @@ try {
   fs.mkdirSync("uploads");
 }
 
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
+
 /**
  * multer는 파일 업로드를 위해 사용되는 multipart/form-data를 다루기 위한 node.js
  * 의 미들웨어 입니다. 하나 혹은 기 이상의 파일을 다루기 위한 모듈입니다.
  *
  * Multer는 파일을 받아서 request에 file혹인 files를 추가하여 넘겨줍니다.
  */
+
 const upload = multer({
-  /* 파일이 저장될 위치 dest or storage*/
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: "nodebird-deploy",
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}${path.basename(file.originalname)}`);
     },
   }),
-  /* 업로드 된 데이터의 한도 */
   limits: { fileSize: 5 * 1024 * 1024 },
 });
+
+// const upload = multer({
+//   /* 파일이 저장될 위치 dest or storage*/
+//   storage: multer.diskStorage({
+//     destination(req, file, cb) {
+//       cb(null, "uploads/");
+//     },
+//     filename(req, file, cb) {
+//       const ext = path.extname(file.originalname);
+//       cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+//     },
+//   }),
+//   /* 업로드 된 데이터의 한도 */
+//   limits: { fileSize: 5 * 1024 * 1024 },
+// });
 
 /*
     업고드 제 옵ㄴ
@@ -56,9 +76,10 @@ const upload = multer({
     static 미들웨어가 /img경로의 정적 파일을 제공하므로 클라이언트에서 업로드한 이미지에 접근할 수 있습니다.
 */
 router.post("/img", isLoggedIn, upload.single("img"), (req, res) => {
-  console.log("req.body POST /post/img", JSON.stringify(req.body, null, 2));
-  console.log(req.file);
-  res.json({ url: `/img/${req.file.filename}` });
+  console.log("req", req);
+  console.log("req.file", req.file);
+  console.log("req.file", req.file.location);
+  res.json({ url: req.file.location });
 });
 
 const upload2 = multer();
